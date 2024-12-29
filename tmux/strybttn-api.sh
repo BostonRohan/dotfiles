e389e52c689f32a80e7a9272be960026a7e1382c
BostonRohan <bostonrohan@gmail.com>
2024-12-29 00:39:08 -0500

    chore: add credits service to storybutton tmux

--- tmux/strybttn-api.sh
+++ tmux/strybttn-api.sh
@@ -2,7 +2,8 @@
 
 SESH="strybttn-api"
 START_SERVER="RUST_LOG=debug cargo watch --workdir apps/bttn_plus_api --env-file=../../.env --ignore schema.graphql -- cargo run --bin bttn_plus_api"
-START_TESTS="RUST_LOG=debug cargo watch --workdir apps/bttn_plus_api --env-file=../../.env.test --ignore schema.graphql -- cargo test -- --nocapture --show-output"
+START_CREDITS="RUST_LOG=debug cargo watch --workdir apps/credits --env-file=../../.env -- cargo run --bin bttn_plus_credits"
+START_TESTS="RUST_LOG=debug ENVIRONMENT=test cargo watch --env-file=.env.test --ignore schema.graphql -- cargo test --workspace -- --nocapture --show-output"
 
 cd ~/Projects/Work/storybutton/bttn_plus_backend/ || exit
 docker compose up -d
@@ -18,6 +19,10 @@ if ! tmux has-session -t $SESH 2>/dev/null; then
   tmux send-keys -t $SESH:server-test.1 "$START_SERVER" C-m
   tmux send-keys -t $SESH:server-test.2 "cd ~/Projects/Work/storybutton/bttn_plus_backend/" C-m
   tmux send-keys -t $SESH:server-test.2 "$START_TESTS" C-m
+
+  tmux new-window -t $SESH -n "credits" -d
+  tmux send-keys -t $SESH:credits "cd ~/Projects/Work/storybutton/bttn_plus_backend/" C-m
+  tmux send-keys -t $SESH:credits "$START_CREDITS" C-m
 
   tmux attach-session -t $SESH
 else
